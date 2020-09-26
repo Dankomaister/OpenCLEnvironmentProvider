9d9aba9dca16e3622179634fb053cc6b4798cd76
Dankomaister <danhed89@gmail.com>
2020-09-26 17:53:20 +0900

    Add files via upload

--- OpenCLEnvironmentProvider/environment.py
+++ OpenCLEnvironmentProvider/environment.py
@@ -6,45 +6,51 @@ from schnetpack.environment import BaseEnvironmentProvider
 class OpenCLEnvironmentProvider(BaseEnvironmentProvider):
 	"""docstring for OpenCLEnvironmentProvider"""
 
-	def __init__(self, cutoff, max_nbh=128):
+	def __init__(self, cutoff, platform=0, number_density=0.25):
 		"""
 		Args:
 			cutoff (float): the cutoff inside which atoms are considered pairs
 		"""
 		self.cutoff = np.float32(cutoff)
-		self.max_nbh = np.uint32(max_nbh)
+		self.max_nbh = np.uint32(round(4/3*np.pi*cutoff**3 * number_density))
 
+		self.kernel_code = ''.join(open('neighbor_list_kernel.cl', 'r').readlines())
+		self.kernel = None
+		self.platform = platform
 		self.mf = cl.mem_flags
-		self.ctx = cl.create_some_context()
-		self.queue = cl.CommandQueue(self.ctx)
 
-		self.kernel = cl.Program(self.ctx, ''.join(open('neighbor_list_kernel.cl', 'r').readlines())).build()
+	def initialize(self):
+		devices = cl.get_platforms()[self.platform].get_devices()
+		self.ctx = cl.Context(devices)
+		self.queue = cl.CommandQueue(self.ctx)
+		self.kernel = cl.Program(self.ctx, self.kernel_code).build()
 
 	def get_environment(self, atoms):
+		if self.kernel is None:
+			self.initialize()
 
 		n_atoms = np.uint32(len(atoms))
-
-		cell = np.array(atoms.get_cell(True), order='C').astype(np.float32)
+		cell = np.array(atoms.cell, order='C').astype(np.float32)
+		positions = np.array(atoms.positions, order='C').astype(np.float32)
 
 		scaled_positions = np.zeros((n_atoms,4), dtype=np.float32, order='C')
-		scaled_positions[:,0:3] = atoms.get_scaled_positions()
+		scaled_positions[:,0:3] = np.linalg.solve(cell.T, positions.T).T
 
 		neighborhood_idx = -np.ones((n_atoms,self.max_nbh), dtype=np.int32, order='C')
 		offset = np.zeros((n_atoms,self.max_nbh), dtype=cl.cltypes.float3, order='C')
 
-		s_buffer = cl.Buffer(self.ctx, self.mf.READ_ONLY | self.mf.COPY_HOST_PTR, hostbuf=scaled_positions)
-		o_buffer = cl.Buffer(self.ctx, self.mf.READ_WRITE | self.mf.COPY_HOST_PTR, hostbuf=offset)
-		nbh_idx_buffer = cl.Buffer(self.ctx, self.mf.READ_WRITE | self.mf.COPY_HOST_PTR, hostbuf=neighborhood_idx)
-		
+		b_scaled_positions = cl.Buffer(self.ctx, self.mf.READ_ONLY | self.mf.COPY_HOST_PTR, hostbuf=scaled_positions)
+		b_offset = cl.Buffer(self.ctx, self.mf.READ_WRITE | self.mf.COPY_HOST_PTR, hostbuf=offset)
+		b_neighborhood_idx = cl.Buffer(self.ctx, self.mf.READ_WRITE | self.mf.COPY_HOST_PTR, hostbuf=neighborhood_idx)
 
-		self.kernel.neighbor_list_cl(self.queue, (n_atoms,), None, s_buffer, o_buffer, nbh_idx_buffer,
+		self.kernel.neighbor_list_cl(self.queue, (n_atoms,), None, b_scaled_positions, b_offset, b_neighborhood_idx,
 			n_atoms, self.max_nbh, self.cutoff,
 			cell[0,0], cell[0,1], cell[0,2],
 			cell[1,0], cell[1,1], cell[1,2],
 			cell[2,0], cell[2,1], cell[2,2])
 
-		e1 = cl.enqueue_copy(self.queue, offset, o_buffer)
-		e2 = cl.enqueue_copy(self.queue, neighborhood_idx, nbh_idx_buffer)
+		e1 = cl.enqueue_copy(self.queue, offset, b_offset)
+		e2 = cl.enqueue_copy(self.queue, neighborhood_idx, b_neighborhood_idx)
 
 		e2.wait()
 		u_idx, n_nbh = np.unique(neighborhood_idx, return_counts=True)
